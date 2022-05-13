aba6af526f55fc41155ad947ba3e64942354f880
rajavasanthan <rajavasanthan@gmail.com>
2022-05-13 11:44:07 +0530

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myfirstangular';
+  timer = 0
+  isButtonDisabled = false;
+  username = "Person 1"
+  constructor(){
+    setTimeout(() => {
+      this.isButtonDisabled = true
+    },3000)
+  }
+
+  postMessage(){
+    alert("Hello World")
+  }
 }
+
+
+// Data Binding
+  // One Way Data Binding
+      // Property Data Binding
+        // Sending Value from TS to HTML
+      // Event Data Binding
+        // Sending Action from HTML to TS
+  
+  // Two Way Data Binding
+    // Both HTML <-> TS
